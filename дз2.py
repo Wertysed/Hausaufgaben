dcd75d30f8781ef3fd4516efd85a7b9ad2c1cc44
Wertysed <wertysed11@gmail.com>
2021-10-23 21:05:33 +0500

    дз2(не полностью)

--- дз2.py
+++ дз2.py
@@ -6,8 +6,9 @@ class Point:
         self.y = y
 
     def length(self, other):
-        print("x other", other.x, "y other", other.y)
+        print("x self", self.x, "y self", self.y, "x other", other.x, "y other", other.y)
         self.length = math.sqrt((other.x - self.x)**2 + (other.y - self.y)**2)
+        print(self.length)
 
 
 class Triangle:
@@ -23,9 +24,9 @@ a = Point(x, y)
 b = Point(x1, y1)
 c = Point(x2, y2)
 #a_treu = Triangle(a.length(b), b.length(c), c.length(a))
-print(a.length(b))
-print(b.length(c))
-print(c.length(a))
+a.length(b)
+b.length(c)
+c.length(a)
 #p = Point(x, y, x1, y1)
 #k = p.length
 #print(a_treu.perimeter, a_treu.plosh)
