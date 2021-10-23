a523d9a4e92ff5fbcbb34d15a2185b8b76abc975
Wertysed <wertysed11@gmail.com>
2021-10-24 00:47:08 +0500

    дз2

--- дз2.py
+++ дз2.py
@@ -1,32 +1,39 @@
 import math
 
 class Point:
+
     def __init__(self, x , y):
         self.x = x
         self.y = y
 
     def length(self, other):
-        print("x self", self.x, "y self", self.y, "x other", other.x, "y other", other.y)
-        self.length = math.sqrt((other.x - self.x)**2 + (other.y - self.y)**2)
-        print(self.length)
+        #print("x self", self.x, "y self", self.y, "x other", other.x, "y other", other.y)
+        length = math.sqrt((other.x - self.x)**2 + (other.y - self.y)**2)
+        return length
+
+
 
 
 class Triangle:
+
     def __init__(self, a, b, c):
-        self.perimeter = a + b + c
-        self.poluper = (a + b + c)/2
-        pep = self.poluper
-        self.plosh = math.sqrt(pep*(pep - a)*(pep - b)*(pep - c))
+        self.a = a
+        self.b = b
+        self.c = c
+
+    def perimeter(self):
+        perimeter = a.length(b) + b.length(c) + c.length(a)
+        return perimeter
+
+    def square(self):
+        pep = (a.length(b) + b.length(c) + c.length(a))/2
+        square = math.sqrt(pep * (pep - a.length(b)) * (pep - b.length(c)) * (pep - c.length(a)))
+        return square
 
 
 x, y, x1, y1, x2, y2 = [int(x) for x in input().split()]
 a = Point(x, y)
 b = Point(x1, y1)
 c = Point(x2, y2)
-#a_treu = Triangle(a.length(b), b.length(c), c.length(a))
-a.length(b)
-b.length(c)
-c.length(a)
-#p = Point(x, y, x1, y1)
-#k = p.length
-#print(a_treu.perimeter, a_treu.plosh)
+z = Triangle(a, b, c)
+print(z.square(), z.perimeter())
